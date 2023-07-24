d3d85d009953c7fcaebb66e31c3b7ac8d2eb3afc
Felipe Bezerra <bzr.lipe@gmai.com>
2023-07-24 13:50:55 -0300

    aula 2

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -1,7 +1,21 @@
 import NavBar from '../../components/Navbar';
+import Menu from '../../components/Menu';
+import banner from './banner.png';
+import styles from './HomePage.module.scss';
 
 export default function HomePage () {
   return (
+    <>
     <NavBar />
+    <main>
+      <section className={styles.principal}>
+        <Menu />
+        <div className={styles.principal__imagem}>  
+          <h1>A galeria mais completa do espaço</h1>
+          <img src={banner} alt='A imagem da terra vista do espaço' />
+        </div>
+      </section>
+    </main>
+    </>
   )
 }
